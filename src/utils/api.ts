6aef3b6ef31d8e2c1e0815fc0a23f7578ab64dff
Asrorbek Aliqulov <asrorbekaliqulov08@gmail.com>
2025-07-17 13:58:31 +0500

    s

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = 'http://127.0.0.1:8830';
 
 // Public API (auth oldidan ishlatiladigan)
 const publicApi = axios.create({
